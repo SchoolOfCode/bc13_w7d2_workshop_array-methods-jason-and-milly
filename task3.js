fcfe000c018e7178e128e0b1ba742f98fc755131
emiliawil <emiliawilliamson@pm.me>
2022-11-08 18:58:42 +0200

    Task 4 complete

--- task3.js
+++ task3.js
@@ -1,1 +1,8 @@
 let someNumbers = [4, 5, 6, 78, 2, 3, 45, 34, 2, 23, 5, 45, 6, 7, 23];
+
+let multipleOfThree = someNumbers.some((number) => number % 3 === 0);
+let multipleOfFive = someNumbers.some((number) => number % 5 === 0);
+let multipleOfSixty = someNumbers.some((number) => number % 60 === 0);
+let multipleOfNinety = someNumbers.some((number) => number % 90 === 0);
+
+console.log(multipleOfThree, multipleOfFive, multipleOfSixty, multipleOfNinety);
--- task4.js
+++ task4.js
@@ -1,3 +1,20 @@
 let sevenTimesTable = [7, 14, 21, 28, 35, 42, 49, 56, 63, 70];
 
+let checkMultiplesOfSeven = sevenTimesTable.every((number) => number % 7 === 0);
+console.log(checkMultiplesOfSeven);
+
 let seventySevenTimesTable = [77, 154, 231, 308, 385, 461, 538, 616, 693, 770];
+let checkMultiplesOfSeventySeven = seventySevenTimesTable.every(
+  (number) => number % 77 === 0
+);
+console.log(checkMultiplesOfSeventySeven);
+
+let rogueNumber = seventySevenTimesTable.find((number) => number % 77 !== 0);
+console.log(rogueNumber);
+
+let rogueIndex = seventySevenTimesTable.findIndex(findRogueIndex);
+console.log(rogueIndex);
+
+function findRogueIndex() {
+  return rogueNumber;
+}
