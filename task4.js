362b4c35d532e558f579095f71408b9890903e76
jasonchalangary <jason42@tuta.io>
2022-11-08 17:24:26 +0000

    completed task 4d

--- task4.js
+++ task4.js
@@ -4,6 +4,7 @@ let checkMultiplesOfSeven = sevenTimesTable.every((number) => number % 7 === 0);
 console.log(checkMultiplesOfSeven);
 
 let seventySevenTimesTable = [77, 154, 231, 308, 385, 461, 538, 616, 693, 770];
+
 let checkMultiplesOfSeventySeven = seventySevenTimesTable.every(
   (number) => number % 77 === 0
 );
@@ -12,9 +13,13 @@ console.log(checkMultiplesOfSeventySeven);
 let rogueNumber = seventySevenTimesTable.find((number) => number % 77 !== 0);
 console.log(rogueNumber);
 
+function findRogueIndex(number) {
+  return number % 77 !== 0
+}
+console.log(seventySevenTimesTable);
 let rogueIndex = seventySevenTimesTable.findIndex(findRogueIndex);
 console.log(rogueIndex);
 
-function findRogueIndex() {
-  return rogueNumber;
-}
+
+
+
